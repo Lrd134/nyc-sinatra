a658fce1f7982977b37b2989a7f635fd65b5f74f
Lrd134 <larryc3200@gmail.com>
2021-06-02 15:41:29 -0400

    for TC

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,33 @@
 class FiguresController < ApplicationController
   # add controller methods
+  get '/figures' do
+    @figures = Figure.all
+    erb :'/figures/index'
+  end
+  get '/figures/new' do
+    @titles = Title.all
+    @landmarks = Landmark.all
+    erb :'/figures/new'
+  end
+  get '/figures/:id/edit' do
+    @figure = Figure.find(params[:id])
+    erb :'/figures/edit'
+  end
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    erb :'/figures/show'
+  end
+  post '/figures' do
+
+   
+      figure = Figure.create(params[:figure])
+      redirect :"/figures/#{figure.id}"
+    
+
+  end
+  patch '/figures/:id' do
+    figure = Figure.find(params[:id])
+    figure.update(params[:figure])
+    redirect :"/figures/#{figure.id}"
+  end
 end
